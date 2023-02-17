ab4c87914c4a80263952f9e1f51b2c257e2afc75
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-02-17 06:14:56 +0100

    feat: add numbers and strings

--- test.js
+++ test.js
@@ -56,9 +56,9 @@ document.write(" you are now over 10");
 console.log(links);
 
 for (i = 0; i < 10; i++) {
-  //console.log(i);
+  // console.log("joj" + i); // 7
 
-  if (i === 5 || i === 3) {
+  if (i === 5 || i === 3) { // 7 jbg
     continue;
   }
   console.log(i);
@@ -90,3 +90,73 @@ function logResult() {
 }
 
 logResult();
+
+
+//numbers
+
+var a = "7";
+var b = 5;
+
+console.log(a + b); //string
+// console.log(typeof (a + b));
+
+console.log(Math.round(7.5)); //Math object
+console.log(Math.ceil(7.8));
+console.log(Math.floor(7.8));
+console.log(Math.max(7, 4, 9, 8)); // max num
+console.log(Math.PI); // max num
+
+//NaN
+
+console.log(a * b); //vraca broj
+
+var a = "apple";
+console.log(a * b); //vraca NaN
+
+if (!isNaN(a)) {
+  console.log("that's not a number");
+} else{
+  console.log("here's the result of a + b = " + (a + b));
+}
+
+
+//string
+
+var myString1 = 'I am a "fun ninja" string';
+var myString2 = 'I\'m a "fun" string'; //escape
+console.log(myString2); 
+console.log(myString1);
+console.log(myString1.length);//string is an object, they have methods and properties - length property
+console.log(myString1.toUpperCase()); //object method
+console.log(myString1.indexOf("string")); //object method //ako ne moze naci vraca -1
+
+if (myString1.indexOf("ninja") === -1) {
+  console.log("the word ninja is not in the string");
+} else{
+  console.log("the word ninja starts at position " + myString1.indexOf("ninja"));
+}
+
+
+var string1 = "abc";
+var string2 = "ABC";
+
+console.log(string1.toLocaleLowerCase() === string2.toLocaleLowerCase());
+
+string2 = "Bcd";
+console.log(string1 < string2); //in js uppercase letter is less than lower
+
+var str = "hello, world";
+console.log(str);
+var str2 = str.slice(2,9); //0 based js, from _ to _
+console.log(str2);
+var str3 = str.slice(2); //0 based js, from _ to end
+console.log(str3);
+
+var tags = "fruit, vegetables, nuts, legumes, grains";
+console.log(tags);
+
+var tagsArray = tags.split(","); // split on comma
+console.log(tagsArray);
+
+
+
